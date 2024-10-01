72b8e36d35311e8adbc7efe78315d59a85880063
Hristo Nikolov <h.nikolov2004@gmail.com>
2024-10-01 13:42:27 +0300

    Time: 120 ms (65.99%), Space: 47.9 MB (40.30%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> hashTable = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (hashTable.ContainsKey(complement))
+            {
+                return new int[] { hashTable[complement], i };
+            }
+
+            hashTable[nums[i]] = i;
+        }
+
+        throw new Exception("Not Found");
+    }
+}
