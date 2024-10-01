35c06050648ed3c88e66962efdd8badade12151b
Hristo Nikolov <h.nikolov2004@gmail.com>
2024-10-01 19:20:59 +0300

    Time: 104 ms (37.85%), Space: 45.1 MB (99.64%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val) // [3,2,2,3], val = 3
+    {
+        int counter = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                continue;
+            }
+
+            (nums[counter], nums[i]) = (nums[i], nums[counter]);
+
+            counter++;
+        }
+
+        return counter;
+    }
+}
