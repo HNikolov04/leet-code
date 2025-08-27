30036f4c982927473d768ea69358b74e0423d0bc
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-27 18:24:51 +0300

    Added Solved 67. Add Binary

---
+++ C#/LeetCode/LeetCode/Challenges/0067-add-binary/0067-add-binary.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode.Challenges._0067_add_binary;
+
+public class _0067_add_binary
+{
+    public string AddBinary(string a, string b)
+    {
+        int firstStringIndex = a.Length - 1;
+        int secondStringIndex = b.Length - 1;
+
+        int carry = 0;
+        int sum = 0;
+
+        string result = string.Empty;
+
+        while (firstStringIndex >= 0 || secondStringIndex >= 0)
+        {
+            sum = carry;
+
+            if (firstStringIndex >= 0)
+            {
+                sum += a[firstStringIndex--] - '0';
+            }
+            if (secondStringIndex >= 0)
+            {
+                sum += b[secondStringIndex--] - '0';
+            }
+
+            if (sum > 1)
+            {
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+
+            result += (sum % 2).ToString();
+        }
+
+        if (carry > 0)
+        {
+            result += carry.ToString();
+        }
+
+        return Reverse(result);
+    }
+
+    private string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+
+        Array.Reverse(charArray);
+
+        return new string(charArray);
+    }
+}
