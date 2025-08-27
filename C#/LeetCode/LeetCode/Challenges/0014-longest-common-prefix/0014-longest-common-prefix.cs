40740773a8926f33564da33001eedb94caeb1ed0
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-27 18:23:36 +0300

    Added Solved 14. Longest Common Prefix

---
+++ C#/LeetCode/LeetCode/Challenges/0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Challenges._0014_longest_common_prefix;
+
+public class _0014_longest_common_prefix
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            string currentPrefix = string.Empty;
+
+            for (int j = 0; j < strs[i].Length; j++)
+            {
+                if (j >= prefix.Length)
+                {
+                    break;
+                }
+
+                if (strs[i][j] == prefix[j])
+                {
+                    currentPrefix += strs[i][j];
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            prefix = currentPrefix;
+        }
+
+        return prefix;
+    }
+}
