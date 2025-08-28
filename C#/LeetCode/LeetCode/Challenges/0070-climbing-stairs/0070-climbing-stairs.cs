616d3133118e6c418c9f13eb391fb6eb83be3c55
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-28 23:40:16 +0300

    Added Solved 70. Climbing Stairs

---
+++ C#/LeetCode/LeetCode/Challenges/0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Challenges._0070_climbing_stairs;
+
+public class _0070_climbing_stairs
+{
+    public int ClimbStairs(int n)
+    {
+        int[] data = new int[n+1];
+        data[0] = 1;
+        data[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            data[i] = data[i-1] + data[i-2];
+        }
+
+        return data[n];
+    }
+}
