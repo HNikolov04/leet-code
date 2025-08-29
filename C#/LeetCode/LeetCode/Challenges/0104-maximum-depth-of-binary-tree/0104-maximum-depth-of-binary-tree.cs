a7723438693e767744e4841c39bdb179c0ad0a4d
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-29 23:18:20 +0300

    Added Solved 104. Maximum Depth of Binary Tree

---
+++ C#/LeetCode/LeetCode/Challenges/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Challenges._0104_maximum_depth_of_binary_tree;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class _0104_maximum_depth_of_binary_tree
+{
+    public int MaxDepth(TreeNode root)
+    {
+        int max = Check(root, 0, 0);
+
+        return max;
+    }
+
+    private int Check(TreeNode node, int level, int maxLevel)
+    {
+        if (node == null)
+        {
+            return Math.Max(level, maxLevel);
+        }
+
+        maxLevel = Check(node.left, level + 1, maxLevel);
+        maxLevel = Check(node.right, level + 1, maxLevel);
+
+        return maxLevel;
+    }
+}
